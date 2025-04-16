8acba61b0af047ed25fcc21efa6394442b6c8a45
gustavomotamacedo <mota.macedo05@gmail.com>
2025-04-16 00:25:31 -0300

    refactor: transformando formulário em componente reativo

--- frontend/src/app/form/form.component.ts
+++ frontend/src/app/form/form.component.ts
@@ -1,14 +1,28 @@
 import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './form.component.html',
   styleUrl: './form.component.scss'
-})
-export class FormComponent {
 
-  getConection() {
-    console.log('getConection');
+})export class FormComponent {
+
+  name = new FormControl('');
+  soluctionId = new FormControl("Selecione uma opção");
+  locationId = new FormControl("Selecione uma opção");
+  formElement: HTMLFormElement = document.createElement('form');
+  formData: object = {};
+
+  constructor() {
+    this.formElement.addEventListener('submit', this.getSolution.bind(this));
+  }
+
+  getSolution($event: any) {
+    $event.preventDefault();
+
+    this.formData = new FormData(this.formElement);
+    console.log(this.formData);
   }
 }
