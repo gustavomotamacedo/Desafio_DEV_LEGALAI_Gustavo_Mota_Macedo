00d9eef4497fdbcb0b90ceb6613135b49a908a0d
gustavomotamacedo <mota.macedo05@gmail.com>
2025-04-15 23:46:56 -0300

    feat: criando rotas para cadastro de usuario e busca de solução associadas ao dataset

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,46 @@
-from flask import Flask
+from flask import Flask, make_response, jsonify, request
 from dataset import tb_usuarios, tb_solucoes
 
 app = Flask(__name__)
 
+@app.route('/')
+def home():
+    return index()
+
 @app.route('/api')
 def index():
     return 'Bem-vindo à API de soluções da LEGAL!'
+
+# === FUNCOES DE SOLUCOES ===
+
+@app.route('/api/solucoes', methods=['GET'])
+def get_solucoes():
+    return make_response(
+        jsonify(tb_solucoes), 200
+    )
+    
+@app.route('/api/solucoes/<int:id>', methods=['GET'])
+def get_solucao(id):
+    for solucao in tb_solucoes:
+        if solucao['id'] == id:
+            return make_response(
+                jsonify(solucao),200
+            )
+
+# === FUNCOES DE USUARIOS ===
+
+@app.route('/api/usuarios', methods=['POST'])    
+def set_usuario():
+    user = request.json
+    tb_usuarios.append(user)
+    return make_response(
+        jsonify(user), 200
+    )
+    
+@app.route('/api/usuarios', methods=['GET'])
+def get_usuarios():
+    return make_response(
+        jsonify(tb_usuarios), 200
+    )
 
 app.run()
