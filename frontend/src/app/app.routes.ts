930ced8cdbe423c17b8dc9a042e4c76e0af642c4
gustavomotamacedo <mota.macedo05@gmail.com>
2025-04-16 00:13:03 -0300

    feat: criando SolutionComponent e criando rota para ele

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { FormComponent } from './form/form.component';
+import { SolutionComponent } from './solution/solution.component';
 
 export const routes: Routes = [
     { path: '', component: FormComponent, pathMatch: 'full' },
+    { path: 'solution', component: SolutionComponent },
 ]
---
+++ frontend/src/app/solution/solution.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-solution',
+  imports: [],
+  templateUrl: './solution.component.html',
+  styleUrl: './solution.component.scss'
+})
+export class SolutionComponent {
+
+}
