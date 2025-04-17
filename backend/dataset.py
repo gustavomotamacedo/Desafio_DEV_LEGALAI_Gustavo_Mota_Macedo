0be51d8d7d03607bf99b6880889fe6b6f8141fc1
gustavomotamacedo <mota.macedo05@gmail.com>
2025-04-16 22:29:24 -0300

    feat: estilizando cards listados e adicionando animações

--- backend/dataset.py
+++ backend/dataset.py
@@ -10,21 +10,21 @@ tb_solucoes = [
 {
     'id': 1,
     'nome': 'FacePass',
-    'descricao': 'Sistema de reconhecimento facial'
+    'descricao': 'Sistema de reconhecimento facial que utiliza inteligência artificial para identificar e autenticar indivíduos de forma rápida e segura, ideal para controle de acesso, monitoramento e personalização de serviços.'
 },
 {
     'id': 2,
     'nome': 'LinkAI',
-    'descricao': 'Matchmaking inteligente com propósito.'
+    'descricao': 'Uma plataforma de matchmaking alimentada por IA que conecta pessoas com base em valores, interesses e objetivos profundos, promovendo relacionamentos significativos e alinhados ao propósito de vida.'
 },
 {
     'id': 3,
     'nome': 'FitScore',
-    'descricao': 'Ferramenta de recrutamento com IA preditiva e alinhamento cultural'
+    'descricao': 'Uma ferramenta de recrutamento que utiliza IA preditiva para avaliar competências técnicas e comportamentais, garantindo o alinhamento cultural entre candidatos e empresas.'
 },
 {
     'id': 4,
     'nome': 'HubX',
-    'descricao': 'Ecossistema digital para o setor de eventos e inovação.'
+    'descricao': 'Plataforma AI que conecta profissionais, empresas e organizadores em um ecossistema digital dinâmico, promovendo networking, inovação e eficiência no setor de eventos.'
 }
 ]
